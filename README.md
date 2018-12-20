2880bc910612396fed1cbf9dc39740e1f6d7c446
zhuxiongxian <929679459@qq.com>
2018-12-20 16:21:34 +0800

    update

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ python3.6.7实现webhook脚本自动部署脚本
 ```
 python3.6.7+flask1.0.2
 ```
+
+#### 将配置信息放在.env文件
+
+将配置信息放在.env文件， 更改为您的`git_path`和`webhook_token`
+
+```
+cp .env_template .env
+```
+
+#### 用法
+
+```
+python app.py &
+# or
+flask run -h 0.0.0.0 &
+```
--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 """
-利用码云的webhook功能实现用户push代码后， 在vps上git pull代码到指定目录下
-
-码云webhook推送数据格式说明: https://gitee.com/help/articles/4186
+利用代码托管库如github/gitlab/bitbucket/gitee等的webhook功能实现用户push代码后， 在vps上git pull代码到指定目录下
 """
 import os
 from flask import Flask, request, Blueprint
@@ -13,23 +11,48 @@ app = Flask(__name__)
 webhook = Blueprint('webhook', __name__, url_prefix='/webhook')
 
 GIT_PATH = os.getenv('git_path', '/home/www/html')
-WEBHOOK_PASSWORD = os.getenv('webhook_password', 'yourwebhookpassword')
+WEBHOOK_TOKEN = os.getenv('webhook_token', 'yourwebhooktoken')
 DEBUG = True if os.getenv('FLASK_ENV', 'development') == 'production' else False
 
 
 @webhook.route('/gitee/', methods=['GET', 'POST'])
 def webhook_gitee():
-    """ gitee webhook """
+    """ gitee webhook 
+
+    利用码云的webhook功能实现用户push代码后， 在vps上git pull代码到指定目录下
+    码云webhook推送数据格式说明: https://gitee.com/help/articles/4186
+    """
     if not request.json:
         return json_error(msg='请求参数不能为空')
 
     password = request.json.get('password', None)
-    if WEBHOOK_PASSWORD == password:
+    if WEBHOOK_TOKEN == password:
         os.chdir(GIT_PATH)
         os.system("git pull")
-        return json_success(msg='git pull完成')
+        return json_success(msg='git pull success')
     else:
-        return json_error(msg='密码不正确')
+        return json_error(msg='Password is wrong')
+
+@webhook.route('/gitlab/', methods=['GET', 'POST'])
+def webhook_gitlab():
+    """ gitlab webhook 
+
+    利用gitlab的webhook功能实现用户push代码后， 在vps上git pull代码到指定目录下
+    gitlab webhook推送数据格式说明: https://gitlab.com/help/user/project/integrations/webhooks
+    """
+    if not request.json:
+        return json_error(msg='请求参数不能为空')
+
+    if 'x-gitlab-token' not in request.headers:
+        return json_error(msg='请求参数不能为空')
+
+    token = request.headers.get('x-gitlab-token', None)
+    if WEBHOOK_TOKEN == token:
+        os.chdir(GIT_PATH)
+        os.system("git pull")
+        return json_success(msg='git pull success')
+    else:
+        return json_error(msg='Password is wrong')
 
 app.register_blueprint(webhook)
 
